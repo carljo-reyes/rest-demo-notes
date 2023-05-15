4aa96d5cd48ed1c23d64353484ac7b7f1d0d1136
Carljo Reyes <carljo.reyes@cambridge.org>
2023-05-16 04:27:34 +0800

    use dotenv to connect to mongo

--- src/backend/database/mongoConn.js
+++ src/backend/database/mongoConn.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
+require('dotenv');
+const MONGO_URI = process.env?.MONGO_URI ??
+    "mongodb://127.0.0.1:27017/rest-demo"
+
 console.log("Connecting to MongoDB");
 
-mongoose.connect("mongodb://127.0.0.1:27017/rest-demo")
+mongoose.connect(MONGO_URI)
     .then(() => { console.log("Connected to MongoDB") })
     .catch(err => console.error(err))
